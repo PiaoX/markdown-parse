fcc393bebb4c7a39a49b66195d7903b5c43754a2
PiaoX <pixie@ucsd.edu>
2022-01-28 16:59:42 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,6 +7,11 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
  @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }}
+    public void getLinksTester() throws IOException {
+        path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        ArrayList<string> links = MarkdownParse.getLinks(contents);
+        assertEquals("Error", new ArrayList<string>(List.of("https://something.com","some-page.html")),links);
+    }
+    
+    }
