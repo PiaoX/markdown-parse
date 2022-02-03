a692838ade429af814d3ed74f973562dde7595d9
PiaoX <97651152+PiaoX@users.noreply.github.com>
2022-02-02 20:29:46 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -18,8 +18,6 @@ public class MarkdownParseTest {
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals("Error", new ArrayList<String>(List.of("https://something.com","some-page.html")),links);
     }
-    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
-
-    java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+   
     
     }
